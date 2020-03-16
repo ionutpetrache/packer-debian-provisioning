11ed2d375fe70939ff23f100b5c24cd64a932752
Ion Petrache <ionpetrache@gmail.com>
2020-03-16 21:08:04 +0100

    Install additional packages: chromium, curl and external programs like docker-ce and visual studio code

---
+++ scripts/install-docker-ce.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+echo Install Docker CE
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker vagrant
---
+++ scripts/install-vscode.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+echo Install Visual Studio Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+
+echo Install a couple of extentions: python, c / c++, docker, robotframework, code runner
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension tomiturtiainen.rf-intellisense
+code --install-extension formulahendry.code-runner
