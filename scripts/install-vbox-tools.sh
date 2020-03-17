cb1c301ba0fc7c736ef0089edb20ccc515bdcd3f
Ion Petrache <ionpetrache@gmail.com>
2020-03-17 11:31:10 +0100

    several small fixes and improvements

--- scripts/install-vbox-tools.sh
+++ scripts/install-vbox-tools.sh
@@ -2,10 +2,13 @@
 
 echo Mount the VBoxGuestAdditions.iso
 mkdir /tmp/isomount
-sudo mount -t iso9660 -o loop /tmp/VBoxGuestAdditions.iso /tmp/isomount
+sudo mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
 
-echo Install VBoxLinuxAdditions
-sudo /tmp/isomount/VBoxLinuxAdditions.run
+echo Install VBoxLinuxAdditions - check for return code 2 as bug on Virtual Box
+sudo /tmp/isomount/VBoxLinuxAdditions.run || [ $? -eq 2 ]
 
 echo Umount VBoxGuestAdditions.iso
 sudo umount /tmp/isomount
+
+echo Checking Virtual Box Kernel Object
+sudo modinfo vboxguest
